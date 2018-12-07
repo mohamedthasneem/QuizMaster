b766d890f1958c6a2f5f05fa7d51defd755a22ff
mohamedthasneem <amythasneem@gmail.com>
2018-12-07 22:00:17 +0530

    Loading Screen Finish with fullscreen

--- app/src/main/java/com/example/thasni/quizemaster/FirstLoadingActivity.java
+++ app/src/main/java/com/example/thasni/quizemaster/FirstLoadingActivity.java
@@ -1,7 +1,10 @@
 package com.example.thasni.quizemaster;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstLoadingActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class FirstLoadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_loading);
+
+        View decorView = getWindow().getDecorView();
+        ((View) decorView).setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(FirstLoadingActivity.this, StartingScreenActivity.class);
+                FirstLoadingActivity.this.startActivity(mainIntent);
+                FirstLoadingActivity.this.finish();
+            }
+        }, 3000);
     }
 }
