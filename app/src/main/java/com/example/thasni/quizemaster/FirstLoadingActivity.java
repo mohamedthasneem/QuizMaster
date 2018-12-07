4c7c4f19c253b678d0d17f66b941749deb3000d5
mohamedthasneem <amythasneem@gmail.com>
2018-12-07 17:26:19 +0530

    Loading screen added

---
+++ app/src/main/java/com/example/thasni/quizemaster/FirstLoadingActivity.java
@@ -0,0 +1,13 @@
+package com.example.thasni.quizemaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstLoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_loading);
+    }
+}
